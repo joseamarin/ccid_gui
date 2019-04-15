77464af4e79f27362f69c3f27da428832bc02c5d
Jose Marin <josemarinemail@gmail.com>
2019-04-15 17:04:19 -0400

    Pushing completed home page to grab the bookmarklet, and switch over to a production link for raw githack

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 (function() {
   const script = document.createElement('script');
-  script.src = 'https://raw.githack.com/joseamarin/ccid_gui/master/app.js';
+  script.src = 'https://rawcdn.githack.com/joseamarin/ccid_gui/c362a1c1511c526133096ead1289249b12a43349/app.js';
   document.body.appendChild(script);
 })();
