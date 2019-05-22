355974357aef278ca50fd50960ba2750deedd80f
Jose Marin <josemarinemail@gmail.com>
2019-05-22 17:54:34 -0400

    Fix for all URI's that contain ampersands

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
+/* globals alert prompt XMLSerializer */
 (function () {
   'use strict';
 
   const anchors = document.querySelectorAll('a');
+  const imgs = document.querySelectorAll('img');
+  const links = document.querySelectorAll('link');
   const cName = 'x' + Math.random().toString(36).substring(2);
   const jsName = 'js-' + Math.random().toString(36).substring(2);
+  const centerClass = 'x' + Math.random().toString(36).substring(2);
 
   const css = `
-.${cName}{display:inline-block!important;border:3px solid currentColor!important;}
+.${cName}{display:inline-block;border:3px solid currentColor;}
+.${centerClass}{padding:10px;font-family:sans-serif;}
   `;
+
+  const createRadio = () => {
+    const html = `
+Remove TBODY elements?
+<input type="radio" id="removeTbody" name="tbody" value="true" checked>
+<label for="true">Yes</label>
+<input type="radio" id="leaveTbody" name="tbody" value="false">
+<label for="false">No</label>
+    `;
+    const element = document.createElement('center');
+    element.classList.add(centerClass);
+    element.innerHTML = html;
+    document.documentElement.insertBefore(element, document.body);
+  };
 
   // add the style tag to the head
   const hlStyle = css => {
@@ -15,16 +34,16 @@
     const style = document.createElement('style');
     head.appendChild(style);
     style.appendChild(document.createTextNode(css));
-  }
+  };
 
   // stop links from going anywhere
-  const pause = anchors => anchors.forEach(a => a.onclick = () => false);
+  const pause = anchors => anchors.forEach(a => { a.onclick = () => false; });
 
   const hl = nodes => {
     nodes.forEach(node => {
-      node.href.includes('ccID') ? node.classList.add(cName) : null;
+      if (node.href.includes('ccID')) node.classList.add(cName);
     });
-  }
+  };
 
   const insertId = anchors => {
     anchors.forEach(anchor => {
@@ -34,60 +53,90 @@
             .match(/ccID=*[0-9]*/gi)[0]
             .substring(anchor.href.match(/ccID=*[0-9]*/gi)[0].length, 5));
           if (input == null) {
-            return;
-          }
-          else if (isNaN(input)) {
+
+          } else if (isNaN(input)) {
             alert('Invalid input');
-          }
-          else if (!input.trim()) {
+          } else if (!input.trim()) {
             alert('Invalid input');
-          }
-          else {
+          } else {
             anchor.href = anchor.href.replace(/ccID=*[0-9]*/gi, 'ccID=');
             input = input.trim();
-            anchor.href = anchor.protocol
-              + '//'
-              + decodeURIComponent(anchor.hostname).toUpperCase()
-              + anchor.pathname + anchor.search + input;
+            anchor.href = anchor.protocol +
+              '//' +
+              decodeURIComponent(anchor.hostname).toUpperCase() +
+              anchor.pathname + anchor.search + input;
           }
         });
       }
     });
-  }
+  };
 
   const getFileName = () => {
     const url = window.location.pathname;
-    const filename = url.substring(url.lastIndexOf('/')+1);
+    const filename = url.substring(url.lastIndexOf('/') + 1);
     return filename;
-  }
+  };
 
   const downloadBtn = () => {
+    const center = document.createElement('center');
     const btn = document.createElement('button');
     const anchor = document.createElement('a');
     anchor.classList.add(jsName);
+    center.classList.add(centerClass);
     btn.innerText = 'Download';
     anchor.setAttribute('download', getFileName());
     anchor.appendChild(btn);
-    document.documentElement.insertBefore(anchor, document.body);
-  }
+    center.appendChild(anchor);
+    document.documentElement.insertBefore(center, document.body);
+  };
 
   pause(anchors);
   hlStyle(css);
   hl(anchors);
   insertId(anchors);
+  createRadio();
   downloadBtn();
 
   const dl = data => {
     const anchor = document.querySelector(`.${jsName}`);
     anchor.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(data));
-    /Gecko\/+\d*/.test(navigator.userAgent) ? anchor.click() : null;
+    if (/Gecko\/+\d*/.test(navigator.userAgent)) anchor.click();
+    setTimeout(() => window.location.reload(), 1000);
   };
+
+  const escape = s => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/&/g, '&amp;');
+
+  const patterns = [];
+  const urls = [];
+
+  const patternsAndReplacements = (anchors, imgs, links) => {
+    anchors.forEach(anchor => {
+      if (!anchor.href.includes('ccID') && anchor.href.includes('&')) {
+        patterns.push(new RegExp(escape(anchor.href), 'g'));
+        urls.push(anchor.href);
+      }
+    });
+    links.forEach(link => {
+      if (link.href.includes('&')) {
+        patterns.push(new RegExp(escape(link.href), 'g'));
+        urls.push(link.href);
+      }
+    });
+    imgs.forEach(img => {
+      if (img.src.includes('&')) {
+        patterns.push(new RegExp(escape(img.src), 'g'));
+        urls.push(img.src);
+      }
+    });
+  };
+
+  patternsAndReplacements(anchors, imgs, links);
 
   const paramify = (obj, amp = '&') => {
     return Object.keys(obj).map(key => {
       return key + '=' + obj[key];
     }).join(amp);
-  }
+  };
 
   const q = {
     eid: '{{EMAIL_USER_ID}}',
@@ -117,15 +166,14 @@
           if (a.href.match(regex)[1] === '') {
             paramObj[key] = '';
             url = paramify(paramObj, amp);
-          }
-          else {
+          } else {
             url = paramify(paramObj, amp);
           }
         }
       });
     });
     return url;
-  }
+  };
 
   const endpoint = checkParams(Object.keys(q), anchors, q, '&');
   const paramsRegex = checkParams(Object.keys(q), anchors, q, '&amp;');
@@ -136,38 +184,54 @@
   }
 
   const convertCharacters = html => {
-    let converted_html = '';
+    let convertedHtml = '';
     let i = 0;
-    while(i < html.length) {
+    while (i < html.length) {
       if (html.charCodeAt(i) > 127) {
-        converted_html += '&#' + html.charCodeAt(i) + ';';
-      }
-      else {
-        converted_html += html.charAt(i);
+        convertedHtml += '&#' + html.charCodeAt(i) + ';';
+      } else {
+        convertedHtml += html.charAt(i);
       }
       ++i;
     }
-    return converted_html;
-  }
+    return convertedHtml;
+  };
 
   const init = () => {
+    let containsTbody = false;
+    if (document.getElementById('removeTbody').checked) {
+      alert('TBODY HTML elements will be removed!');
+      containsTbody = true;
+    } else {
+      alert('TBODY HTML elements will not be removed!');
+    }
+
+    document.querySelector('.' + centerClass).parentNode
+      .removeChild(document.querySelector('.' + centerClass));
+
     anchors.forEach(a => a.classList.remove(cName));
     const html = document.documentElement.outerHTML;
     const fullDocument = doctype !== undefined ? doctype + '\n' + html : html;
-
     const regex = new RegExp(paramsRegex, 'gim');
     const cssRegex = new RegExp('<style>' + css + '</style>', 'gim');
     const jsNameRegex = new RegExp(`<a class="${jsName}">?.*</a>`, 'g');
+    const radioRegex = new RegExp(`<center class="${centerClass}">?.*</center>`, 'g');
 
     let source = convertCharacters(fullDocument);
+    if (containsTbody) {
+      source = source.replace(/<tbody>|<\/tbody>/g, '');
+    }
+    patterns.forEach((pattern, idx) => {
+      source = source.replace(pattern, urls[idx]);
+    });
+    source = source.replace(radioRegex, '');
     source = source.replace(regex, endpoint);
     source = source.replace(cssRegex, '');
     source = source.replace(jsNameRegex, '');
-    source = source.replace(/class=""/gi, '');
-    source = source.replace(/<script src="https:\/\/rawcdn.githack.com\/>?.*<\/script>/gmi, '');
+    source = source.replace(/class=""/g, '');
 
     dl(source);
-  }
+  };
 
   document.querySelector('.' + jsName).addEventListener('click', e => {
     init();
--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   const script = document.createElement('script');
   script.src = 'https://rawcdn.githack.com/joseamarin/ccid_gui/c362a1c1511c526133096ead1289249b12a43349/app.js';
   document.body.appendChild(script);
